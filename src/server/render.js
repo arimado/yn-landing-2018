fa83086e34e13a90479e89d2abf56e0496d56104
Jeremy Arimado <jeremyarimado@Jeremys-MacBook-Pro.local>
2018-08-14 21:07:44 +1000

    saving file with same dimensions as canvas

--- src/server/render.js
+++ src/server/render.js
@@ -2,21 +2,20 @@ import fs from "fs";
 import path from "path";
 import Canvas, { Image } from "canvas";
 
-const canvas = new Canvas(200, 200);
-const ctx = canvas.getContext('2d');
-
 // From a buffer:
 fs.readFile(path.join(__dirname, "/images/img.jpg"), (err, src) => {
   if (err) throw err;
   const img = new Image();
-  console.log(ctx);
-  console.log(Object.keys(ctx))
-  img.onload = () => ctx.drawImage(img, 0, 0);
   img.onerror = err => {
     throw err;
   };
+  img.onload = () => {
+    const canvas = new Canvas(img.width, img.height);
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    console.log("write file");
+    const buf = canvas.toBuffer();
+    fs.writeFileSync("canvas2.png", buf);
+  };
   img.src = src;
-
-  const buf = canvas.toBuffer();
-  fs.writeFileSync("canvas.png", buf);
 });
