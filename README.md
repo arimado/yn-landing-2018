6207b12b568a7b90cb3435525ee123b0f2b5d760
Jeremy Arimado <jeremyarimado@Jeremys-MacBook-Pro.local>
2018-09-01 16:30:05 +1000

    added fluent ffmpeg

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Make sure you have the following on your machine
 
 `brew install pkg-config cairo pango libpng jpeg giflib` 
 
+`brew install ffmpeg $(brew options ffmpeg | grep -vE '\s' | grep -- '--with-' | tr '\n' ' ')`
+from https://gist.github.com/Piasy/b5dfd5c048eb69d1b91719988c0325d8
+
--- src/server/render.js
+++ src/server/render.js
@@ -3,19 +3,22 @@ import path from "path";
 import Canvas, { Image } from "canvas";
 
 // From a buffer:
-fs.readFile(path.join(__dirname, "/images/img.jpg"), (err, src) => {
-  if (err) throw err;
-  const img = new Image();
-  img.onerror = err => {
-    throw err;
-  };
-  img.onload = () => {
-    const canvas = new Canvas(img.width, img.height);
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    console.log("write file");
-    const buf = canvas.toBuffer();
-    fs.writeFileSync("canvas2.png", buf);
-  };
-  img.src = src;
-});
+
+const readSingleImage = () => {
+  fs.readFile(path.join(__dirname, "/images/img.jpg"), (err, src) => {
+    if (err) throw err;
+    const img = new Image();
+    img.onerror = err => {
+      throw err;
+    };
+    img.onload = () => {
+      const canvas = new Canvas(img.width, img.height);
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      console.log("write file");
+      const buf = canvas.toBuffer();
+      fs.writeFileSync("canvas2.png", buf);
+    };
+    img.src = src;
+  });
+}
